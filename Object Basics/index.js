80581c6ceb69baa36d46d7f358d55deda0acbe6a
SethJKallis <kallisseth@gmail.com>
2022-11-14 16:35:58 +0200

    added lessons on object basics and factory functions

---
+++ Object Basics/index.js
@@ -0,0 +1,39 @@
+// OBJECT BASICS
+let person = {
+    name : `Seth`,
+    surname : `Kallis`,
+    age : 22
+}
+// console.log(person.name)
+// console.log(person[`surname`])
+
+person.email = `email@gmail.com`
+// console.log(person)
+
+
+
+
+// FACTORY FUNCTION
+// A function that returns an object.
+
+// DISPLAYING A USERS NAME AND SURNAME
+function userDetails(firstName, lastName){
+    return{
+        firstName : firstName,
+        lastName : lastName, 
+        display(){
+            console.log(`Name: ${firstName}\nSurname: ${lastName}`)
+        }
+        
+}
+}
+
+// CREATE AN INSTANCE
+let person1 = userDetails(`Joel`, `Something`);
+person1.firstName = `Seth`;
+person1.display();
+
+console.log(`=================`)
+
+const person2 = userDetails(`Seth`, `Kallis`);
+person2.display()
