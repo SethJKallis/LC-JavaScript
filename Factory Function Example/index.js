49c561654816d854d30c461f66a8bf39cf97ad38
SethJKallis <kallisseth@gmail.com>
2022-11-15 09:46:39 +0200

    added a factory function example for presentation in class

---
+++ Factory Function Example/index.js
@@ -0,0 +1,91 @@
+// function vehicles(make, year, color){
+//     return{
+//         make : make,
+//         year : year,
+//         color : color,
+//         display(){
+//             console.log(`Make: ${make}\nYear: ${year}\nColor: ${color}`)
+//         },
+//         rev() {
+//             console.log("Vroom! Vroom!");
+//         }
+//     }
+// }
+
+// let BMW = vehicles("M3", 2016, "Black")
+// BMW.display()
+
+function vehicles(config){
+    return{
+        make : config.make,
+        year : config.year,
+        color : config.color,
+        displayInfo(){
+            console.log(`Make: ${this.make}\nYear: ${this.year}\nColor: ${this.color}`)
+        },
+        displayCar(){
+            if (this.color == 'blue') {
+                console.log('🚙')
+            }else if (this.color == 'red') {
+                console.log('🚗');             
+            }else if (this.color == 'white') {
+                console.log('🚓')
+            } else {
+                console.log('👲')
+            } 
+        },
+        rev() {
+            if (this.make == "no car") {
+                console.log("🎵Better run, better run! Outrun my Gun!🎵");
+            } else {
+                console.log("Vroom! Vroom!");
+            }
+        }
+    }
+}
+
+let BMW = vehicles({
+    make: "M3",
+    year:  2016,
+    color: "red"
+})
+BMW.displayInfo()
+BMW.displayCar()
+BMW.rev()
+
+console.log('---------------');
+
+let nissan = vehicles({
+    make: "GTR",
+    year: 2014,
+    color: "blue"
+})
+nissan.displayInfo()
+nissan.displayCar()
+nissan.rev()
+
+console.log('---------------');
+
+let subaru = vehicles({
+    make: "Impreza",
+    year: 2014,
+    color: "white"
+})
+subaru.displayInfo()
+subaru.displayCar()
+subaru.rev()
+
+console.log('---------------');
+
+
+let man = vehicles({
+    make: "no car",
+    year: 1200 + "BC",
+    color: "asian",
+})
+
+man.displayInfo()
+man.displayCar()
+man.rev()
+
+console.log('---------------');
--- Object Basics/index.js
+++ Object Basics/index.js
@@ -16,26 +16,30 @@ person.email = `email@gmail.com`
 // FACTORY FUNCTION
 // A function that returns an object.
 // A template for instances
+// A feature that we need to implement
+// Place intended code inside the return{}
 
 // DISPLAYING A USERS NAME AND SURNAME
-function userDetails(firstName, lastName){
+function userDetails(firstName, lastName, age){
     return{
         firstName : firstName,
         lastName : lastName, 
+        age : age,
         display(){
-            console.log(`Name: ${firstName}\nSurname: ${lastName}`)
+            console.log(`Name: ${firstName}\nSurname: ${lastName}\nAge: ${age}`)
         }
 }
 }
 
-// CREATE AN INSTANCE
-// 
 
-let person1 = userDetails(`Joel`, `Something`);
-person1.firstName = `Seth`;
+// ******BACK-STICK TO MAKE USE OF A PLACEHOLDER IN A STRING******
+
+// CREATE AN INSTANCE
+
+let person1 = userDetails(`Joel`, `Something`, 30);
 person1.display();
 
 console.log(`=================`)
 
-const person2 = userDetails(`Seth`, `Kallis`);
+const person2 = userDetails(`Seth`, `Kallis`, 22);
 person2.display()
